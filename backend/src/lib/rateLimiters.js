46bd0b444a4f92a136549811f25f5c055285d89e
Arivu L <144206458+Arivu27032005@users.noreply.github.com>
2025-07-17 20:33:26 +0530

    Update rateLimiters.js

--- backend/src/lib/rateLimiters.js
+++ backend/src/lib/rateLimiters.js
@@ -2,7 +2,7 @@ import rateLimit from 'express-rate-limit';
 
 export const authLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 7,
+  max: 10,
   message: 'Too many login attempts, please try again later'
 });
 
