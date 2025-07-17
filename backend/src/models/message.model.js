cf0cdf1b5356d65d1b4c67290d974e5af10b0639
Arivu L <144206458+Arivu27032005@users.noreply.github.com>
2025-07-17 07:10:32 +0530

    Update message.model.js

--- backend/src/models/message.model.js
+++ backend/src/models/message.model.js
@@ -23,8 +23,6 @@ const messageSchema=new mongoose.Schema(
             type:String,
         },
 
-        // Important message handling
-
         messageType:{
             type:String,
             enum:["Normal", "ShouldReply", "StrictReply"],
